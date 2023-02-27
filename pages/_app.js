1b3933c27cb4bc55413a3710ca4e6aa8f2e0abb6
Moritz Löchner <momo.loechner@gmail.com>
2023-02-27 11:00:17 +0100

    react-query init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Footer from '../components/footer';
+import Navbar from '../components/navbar';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient();
+
+export default function App({ Component, pageProps }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </QueryClientProvider>
+  );
 }
-
-export default MyApp
